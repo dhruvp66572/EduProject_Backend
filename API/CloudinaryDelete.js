11fa7f6c594127a496ec2a9151ea0ac8c337deee
Dhruv Prajapati <dhruvprajapati66572@gmail.com>
2024-09-20 15:13:46 +0530

    Add Dependancey for Cloudinary

--- API/CloudinaryDelete.js
+++ API/CloudinaryDelete.js
@@ -1,4 +1,4 @@
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
 const process = require("process");
 const { Router } = require("express");
